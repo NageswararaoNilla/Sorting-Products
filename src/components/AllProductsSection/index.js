5f50ddf545f13401000c1d3514d722922c286c9d
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-11 10:55:55 +0000

    session completed

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loader-spinner'
 import Cookies from 'js-cookie'
 
 import ProductCard from '../ProductCard'
+import ProductsHeader from '../ProductsHeader'
 import './index.css'
 
 const sortbyOptions = [
@@ -18,6 +19,7 @@ const sortbyOptions = [
 
 class AllProductsSection extends Component {
   state = {
+    activeOptionId: sortbyOptions[0].optionId,
     productsList: [],
     isLoading: false,
   }
@@ -30,8 +32,9 @@ class AllProductsSection extends Component {
     this.setState({
       isLoading: true,
     })
+    const {activeOptionId} = this.state
     const jwtToken = Cookies.get('jwt_token')
-    const apiUrl = 'https://apis.ccbp.in/products'
+    const apiUrl = `https://apis.ccbp.in/products?sort_by=${activeOptionId}`
     const options = {
       headers: {
         Authorization: `Bearer ${jwtToken}`,
@@ -56,11 +59,22 @@ class AllProductsSection extends Component {
     }
   }
 
+  updateActiveOptionId = activeOptionId => {
+    // this.setState({activeOptionId})
+    // this.getProducts()
+    this.setState({activeOptionId}, this.getProducts)
+  }
+
   renderProductsList = () => {
-    const {productsList} = this.state
+    const {productsList, activeOptionId} = this.state
     return (
       <>
-        <h1 className="products-heading">All Products</h1>
+        {/* <h1 className="products-heading">All Products</h1> */}
+        <ProductsHeader
+          sortbyOptions={sortbyOptions}
+          activeOptionId={activeOptionId}
+          updateActiveOptionId={this.updateActiveOptionId}
+        />
         <ul className="products-list">
           {productsList.map(product => (
             <ProductCard productData={product} key={product.id} />
